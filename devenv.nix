2569e8e39355e68f52867a7dc51ce726289f9d2d
Wolfgang Walther <walther@technowledgy.de>
2025-01-24 20:21:47 +0100

    no-substitute-all: init

--- devenv.nix
+++ devenv.nix
@@ -25,8 +25,27 @@ in
 
   languages.nix.enable = true;
 
+  scripts.no-substitute-all = {
+    exec = ''
+      def main [nixpkgs: string, --write (-w)] {
+          if $write {
+              ast-grep scan --rule rules/no-substitute-all-nix.yml $nixpkgs --update-all
+          } else {
+              ast-grep scan --rule rules/no-substitute-all-nix.yml $nixpkgs
+          }
+      }
+    '';
+    package = pkgs.nushell;
+    binary = "nu";
+    description = "Find (and replace) all substituteAll calls in nixpkgs.";
+  };
+
   enterShell = ''
     ln -sf ${sgconfig} sgconfig.yml
+
+    echo "Available scripts:"
+    echo "  no-substitute-all <path-to-nixpkgs>"
+    echo "  no-substitute-all <path-to-nixpkgs> --write"
   '';
 
   enterTest = ''
